d05934d662ab2ad843e21dfd2d4caa9dea893eab
sazcurraa25 <aazcurrasantiagoo@gmail.com>
2025-08-22 16:53:16 -0300

    resolucion tareas 1

--- Ejercicios-1/ejercicios-1/ejercicio-3.py
+++ Ejercicios-1/ejercicios-1/ejercicio-3.py
@@ -1,11 +1,12 @@
 sumOfNumbers= 0 
 while True: 
     number= int(input("Ingrese un número: "))
+    answer = input("Deseas agregar otro número? (si/no): ").lower()
     if number % 2 == 0:
-        sumOfNumbers +=0
-    answer = input("Deseas agregar otro número? (si/no): ").lower
+        sumOfNumbers += number
     if answer == "no":
         break
+    elif answer != "si":
+        print("Lo introducido no corresponde con lo que se pide")
 print("La suma de los numeros pares es", sumOfNumbers)
 
-#linea de codigo nueva 
--- Ejercicios-1/ejercicios-1/ejercicio-4.py
+++ Ejercicios-1/ejercicios-1/ejercicio-4.py
@@ -1,14 +1,21 @@
-numberToKnow=(int(input("Ingresa el numero a adivinar: ")))
-tryToKnowNumber=(int(input("Ingresa un número: ")))
 countOfNumbers= 0
-while(numberToKnow, tryToKnowNumber):
+while True:
+    numberToKnow=(int(input("Ingresa el numero a adivinar: ")))
+    tryToKnowNumber=(int(input("Ingresa un número: ")))
     tryOtherNumber=(int(input("Ingresa otro número: ")))
-    countOfNumbers=countOfNumbers+1
-    if(numberToKnow>tryToKnowNumber):
+    if numberToKnow>tryToKnowNumber:
         print("El número secreto es más chico. Intente nuevamente")
-    if(numberToKnow<tryToKnowNumber):
+        countOfNumbers=countOfNumbers+1
+    elif numberToKnow<tryToKnowNumber:
         print("El número secreto es más grande. Intente nuevamente")
-    if(numberToKnow==tryToKnowNumber):
+        countOfNumbers=countOfNumbers+1
+    elif numberToKnow==tryToKnowNumber:
         print("Acertó al número secreto")
         print("Uso", countOfNumbers, "intentos para adivinar")
         break
+    elif numberToKnow != int():
+        print("El valor entregado no es valido, intente nuevamente")
+    elif tryOtherNumber != int():
+        print("El valor entregado no es valido, intente nuevamente")
+    elif tryToKnowNumber != int():
+        print("El valor entregado no es valido, intente nuevamente")
