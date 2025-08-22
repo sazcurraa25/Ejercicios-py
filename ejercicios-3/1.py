282124899153c554ca3a7f8ede27027c80bc42fa
sazcurraa25 <aazcurrasantiagoo@gmail.com>
2025-08-21 21:02:13 -0300

    ejercicios 21-08

---
+++ ejercicios-3/1.py
@@ -0,0 +1,9 @@
+def comparison(first_word, second_word):
+    first_word=input("Inresá una palabra: ")
+    second_word=input("Inglesa una palabra: ")
+    if first_word==second_word:
+        print("True")
+    else:
+        print("False")
+    return
+print(comparison)
