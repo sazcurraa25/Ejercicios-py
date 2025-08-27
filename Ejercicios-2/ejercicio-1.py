1502c03f339c143f595dd81bdbf8dda001c0f990
sazcurraa25 <aazcurrasantiagoo@gmail.com>
2025-08-27 10:03:09 -0300

    ejercicios 1 y 2

---
+++ Ejercicios-2/ejercicio-1.py
@@ -0,0 +1,5 @@
+DataBase=[43, 65, 26, 74, 24, 62, 51, 61, 45, 63, 26, 62, 2,34 , 62, 26, 66, 62]
+print(min(DataBase))
+print(max(DataBase))
+print((sum(DataBase))/len(DataBase))
+print(len(DataBase))
---
+++ Ejercicios-2/ejercicio-2.py
@@ -0,0 +1,28 @@
+numberOfParticipants=0
+nonSatisfactory=[]
+lessSatisfactory=[]
+mediumSatisfactory=[]
+goodSatisfactory=[]
+excellentSatisfactory=[]
+while (numberOfParticipants < 15):
+    getQualification=int(input("Exprese su satisfacción del 1 al 5: "))
+    if getQualification == 1:
+        nonSatisfactory.append(getQualification)
+    elif getQualification == 2:
+        lessSatisfactory.append(getQualification)
+    elif getQualification == 3:
+        mediumSatisfactory.append(getQualification)
+    elif getQualification == 4: 
+        goodSatisfactory.append(getQualification)
+    elif getQualification == 5:
+        excellentSatisfactory.append(getQualification)
+    else:
+        print("El numero ingresado no es correcto, intente de nuevo")
+        continue
+    numberOfParticipants +=1
+
+print("Nada Satisfecho:",((len(nonSatisfactory)/15)*100), "%")
+print("Poco satisfecho:",((len(lessSatisfactory)/15)*100), "%")
+print("Medianamente satisfecho:",((len(mediumSatisfactory)/15)*100), "%")
+print("Satisfecho:",((len(goodSatisfactory)/15)*100), "%")
+print("Muy satisfecho:",((len(excellentSatisfactory)/15)*100),"%")
