2a5065ce78e02377fb01b4d691d6002e1dd71eba
sazcurraa25 <aazcurrasantiagoo@gmail.com>
2025-09-04 23:43:44 -0300

    resolucion-ojetos-ej-1

--- ejercicios-5/ejercicio_1.py
+++ ejercicios-5/ejercicio_1.py
@@ -10,10 +10,10 @@ class  Rectangulo():
         self.ladoB = valor
     def area(self):
         self.area = self.ladoA * self.ladoB
-        print("El area es ", self.area )
+        print("El area de este rectangulo es ", self.area )
     def perimetro(self):
         self.perimetro = 2*(self.ladoA + self.ladoB)
-        print("El perimetro es ", self.perimetro)
+        print("El perimetro de este rectangulo es ", self.perimetro)
 
 rectangulo1 = Rectangulo()
 rectangulo1.cuantoValeA(7.5)
@@ -25,9 +25,24 @@ class triangulo:
     cateto1= 0 
     cateto2= 0
     hipotenusa = 0
-    def cuantovaleCateto(self, valor):
+    area = 0
+    perimetro = 0
+    def cuantovaleCateto1(self, valor):
         self.cateto1 = valor
-    def cuantoValeCateto(self, valor):
+    def cuantoValeCateto2(self, valor):
         self.cateto2 = valor
+    def cuantoValeHipotenusa(self, valor):
+        self.hipotenusa = valor
     def area(self):
-        StopAsyncIteration
+        self.area = (self.cateto1 * self.cateto2)/2
+        print("El area de este triangulo es ", self.area)
+    def perimetro(self):
+        self.perimetro= self.cateto1 + self.cateto2 + self.hipotenusa
+        print("El perimetro de este triangulo es ", self.perimetro)
+
+triangulo1 = triangulo()
+triangulo1.cuantovaleCateto1(12)
+triangulo1.cuantoValeCateto2(10)
+triangulo1.cuantoValeHipotenusa(27)
+triangulo1.area()
+triangulo1.perimetro()
